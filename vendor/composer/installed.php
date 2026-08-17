15ed4cc2cb0661b9194ed22acf9a7d6d1ac8cf55
MJSantos <mjsantoslx@gmail.com>
2026-08-17 17:27:15 +0100

    V5-A

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'siga/siga-mvc',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'a108575a6c0e1ef2f7bb12ef3be73ccee005d84f',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'siga/siga-mvc' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'a108575a6c0e1ef2f7bb12ef3be73ccee005d84f',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
